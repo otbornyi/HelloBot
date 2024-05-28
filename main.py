dd027744a1379b2067db8515b4be1890b84e0029
Razil <kontra9797@mail.ru>
2024-05-28 23:10:42 +0300

    Взял токен , добавил метод который отвечает на первое  действие пользователя(отправляет текст)

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import telebot
 
-bot = telebot.TeleBot("")
+bot = telebot.TeleBot('7463175667:AAGrh1Qy_VmeGc-xCcgIFSx4_HaoxubJf0o') # токен берем у BotFather
+
+
+@bot.message_handler(commands = ['start'])
+def start(message): # Метод который отвечает на определенное сообщение пользователя
+    bot.send_message(message.chat.id, "<b>Привет пользователь!</b>", parse_mode='html') # Что оправляем(в какой чат отсылаем, само сообщение, режим в котором отправляем текст)
+
+
+
+bot.polling(none_stop=True)
